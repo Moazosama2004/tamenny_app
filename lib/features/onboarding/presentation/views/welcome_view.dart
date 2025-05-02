96efef70bfb621001fa8e1720e67d9156d5f0980
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-05-02 14:52:05 +0300

    some changes

--- lib/features/onboarding/presentation/views/welcome_view.dart
+++ lib/features/onboarding/presentation/views/welcome_view.dart
@@ -1,60 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:tamenny_app/core/theme/app_colors.dart';
-import 'package:tamenny_app/core/theme/app_styles.dart';
-import 'package:tamenny_app/core/utils/app_assets.dart';
-
-import '../../../../core/widgets/already_have_an_account.dart';
-import 'widgets/get_started_custom_button.dart';
-import 'widgets/header_logo_welcome_view.dart';
+import 'package:tamenny_app/features/onboarding/presentation/views/widgets/welcome_view_body.dart';
 
 class WelcomeView extends StatelessWidget {
   const WelcomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
-            children: [
-              const SizedBox(
-                height: 24,
-              ),
-              const HeaderLogoWelcomeView(),
-              const SizedBox(
-                height: 16,
-              ),
-              Text(
-                'Welcome to the TAMENNY',
-                textAlign: TextAlign.center,
-                style: AppStyles.font30ExtraBold.copyWith(
-                  color: AppColors.primaryColor,
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              const Text(
-                'Your mindful mental health AI companion for everyone, anywhere 🍃',
-                textAlign: TextAlign.center,
-                style: AppStyles.font18Medium,
-              ),
-              const SizedBox(
-                height: 32,
-              ),
-              Image.asset(Assets.imagesOnboarding1),
-              const SizedBox(
-                height: 32,
-              ),
-              const GetStartedButton(),
-              const SizedBox(
-                height: 30,
-              ),
-              const AlreadyHaveAnAccount()
-            ],
-          ),
-        ),
+        body: WelcomeViewBody(),
       ),
     );
   }
---
+++ lib/features/onboarding/presentation/views/widgets/welcome_view_body.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:tamenny_app/core/theme/app_colors.dart';
+import 'package:tamenny_app/core/theme/app_styles.dart';
+import 'package:tamenny_app/core/utils/app_assets.dart';
+import 'package:tamenny_app/core/widgets/already_have_an_account.dart';
+import 'package:tamenny_app/features/onboarding/presentation/views/widgets/get_started_custom_button.dart';
+import 'package:tamenny_app/features/onboarding/presentation/views/widgets/header_logo_welcome_view.dart';
+
+class WelcomeViewBody extends StatelessWidget {
+  const WelcomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: ListView(
+        children: [
+          const SizedBox(
+            height: 24,
+          ),
+          const HeaderLogoWelcomeView(),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Welcome to the TAMENNY',
+            textAlign: TextAlign.center,
+            style: AppStyles.font30ExtraBold.copyWith(
+              color: AppColors.primaryColor,
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const Text(
+            'Your mindful mental health AI companion for everyone, anywhere 🍃',
+            textAlign: TextAlign.center,
+            style: AppStyles.font18Medium,
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          Image.asset(Assets.imagesOnboarding1),
+          const SizedBox(
+            height: 32,
+          ),
+          const GetStartedButton(),
+          const SizedBox(
+            height: 30,
+          ),
+          const AlreadyHaveAnAccount()
+        ],
+      ),
+    );
+  }
+}
